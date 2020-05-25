bf23d0fde982f39c8d16f628cd1768aeac78b3e4
Joe Cocco <cocco3@gmail.com>
2020-05-24 20:54:42 -0700

    Updates

---
+++ precache-manifest.ba5e387f99731cc09009c6080f179751.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5733dfd0e0350619d1c4303d5eb30bd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5937e7fe1e5b7c470f32",
+    "url": "/static/css/main.9bddb300.chunk.css"
+  },
+  {
+    "revision": "7d593e9be7f19017a8d7",
+    "url": "/static/js/2.eefcbae9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eefcbae9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5937e7fe1e5b7c470f32",
+    "url": "/static/js/main.e3150fa7.chunk.js"
+  },
+  {
+    "revision": "62604699ccb7aefe30f8",
+    "url": "/static/js/runtime-main.f885d42f.js"
+  }
+]);
