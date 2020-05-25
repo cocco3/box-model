1f1acdba37560ebdf9e9f0f86d40f942f16df232
Joe Cocco <cocco3@gmail.com>
2020-05-24 21:16:28 -0700

    Updates

---
+++ precache-manifest.86c8a9b67c89b0997db791f748220cf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5808ecaf55225c58bb544103cc7926d",
+    "url": "/box-model/index.html"
+  },
+  {
+    "revision": "3ba24251a7cd80ec8d45",
+    "url": "/box-model/static/css/main.9bddb300.chunk.css"
+  },
+  {
+    "revision": "d8f1b7e6d0ebf80b55ca",
+    "url": "/box-model/static/js/2.31f20920.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/box-model/static/js/2.31f20920.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ba24251a7cd80ec8d45",
+    "url": "/box-model/static/js/main.7722e1a4.chunk.js"
+  },
+  {
+    "revision": "a0ac83462aeb8b6981ce",
+    "url": "/box-model/static/js/runtime-main.c9402ce4.js"
+  }
+]);
